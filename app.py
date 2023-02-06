a113a4bf4d5b53855ec8bfac701a0871bb6bdf81
Chanbyeongee <well87865@gmail.com>
2023-02-07 00:10:27 +0900

    :ambulance:!HOTFIX: swagger 모델 수정

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def mock():
 if __name__ == "__main__":
     from resources import create_api,create_api_models
 
-    #create_api_models(api)
+    create_api_models(api)
     create_api(api)
     #create_socketio(sock)
 
