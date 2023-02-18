e10f32a029e62eee046961993be0f4f5fee55d47
Chanbyeongee <well87865@gmail.com>
2023-02-19 03:49:42 +0900

    :art:Style: 쓰지 않는 라이브러리 삭제

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from flask_restx import Api, fields
 from flask_jwt_extended import JWTManager
-from flask_socketio import SocketIO
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from db import db
